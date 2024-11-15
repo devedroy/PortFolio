c33f0270523e8b4eae9cccd1aa6e41233adb8e25
devedroy <devedroy.dr@gmail.com>
2024-11-15 18:14:49 +0530

    optimize imports

--- composeApp/src/wasmJsMain/kotlin/io/github/devedroy/Header.kt
+++ composeApp/src/wasmJsMain/kotlin/io/github/devedroy/Header.kt
@@ -1,7 +1,6 @@
 package io.github.devedroy
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
@@ -9,7 +8,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
